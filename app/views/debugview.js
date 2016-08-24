b275834d6ab8fd48f4cd458a66a6307ab4b59a6f
Philipp Koytek <Philipp Koytek>
2016-08-24 16:38:29 -0600

    made debug view interactive

--- app/views/debugview.js
+++ app/views/debugview.js
@@ -1,6 +1,7 @@
 class DebugView extends View {
     constructor (width, height, position){
         super('debug',width, height, position, {top:10, right:10, bottom:10, left:10});
+        var self = this;
         this.text = this.chart.append('foreignObject')
             .attr({width:width-20, height:height-20})
             .append('xhtml:div')
@@ -8,7 +9,11 @@ class DebugView extends View {
                 height:''+(height-20)+'px',
                 overflow: 'auto'
             })
-            .text('debug outputs');
+            .text('debug outputs')
+            .on('click', function(){
+                d3.select(this).selectAll('*').remove();
+                self.log(JSON.stringify(DATA[0]));
+            });
     }
 
     log(text){
