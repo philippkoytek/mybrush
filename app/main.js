85de820916e0a9a4dcf4a69667e042b63f2ebd38
Philipp Koytek <Philipp Koytek>
2016-08-25 11:20:36 -0600

    global array of views

--- app/main.js
+++ app/main.js
@@ -4,15 +4,16 @@
 
 var DEBUG;
 var DATA;
+var VIEWS = [];
 
 Data.request('data/fifaplayers-top50.json', 'fifaplayers', function(error, data, key) {
 
     //TODO: positioning of views, make them draggable?
     DATA = data = [data[0], data[1]];
 
-    var scatterplot = new ScatterPlot('likes', 'dislikes', 700, 400, {x: 20, y: 20}).data(data);
-    var scatterplot2 = new ScatterPlot('likes', 'dislikes', 700, 400, {x: 20, y: 440}).data(data);
-    var scatterplot3 = new ScatterPlot('likes', 'dislikes', 700, 400, {x: 740, y: 20}).data(data);
+    VIEWS.push(new ScatterPlot('likes', 'dislikes', 700, 400, {x: 20, y: 20}).data(data));
+    VIEWS.push(new ScatterPlot('likes', 'dislikes', 700, 400, {x: 20, y: 440}).data(data));
+    VIEWS.push(new ScatterPlot('likes', 'dislikes', 700, 400, {x: 740, y: 20}).data(data));
     DEBUG = new DebugView(700, 400, {x:740, y:440});
     // var barchart = new BarChart('number of players', 700, 400, {x: 20, y: 440}).data(data);
     // var parallelcoords = new ParallelCoords(700, 400, {x: 740, y: 20}).data(data);
