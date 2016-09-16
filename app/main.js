e535b2ce6b78dfe6e1126fdc0fe74a308d03030f
AUTOBVT-UPIDP8V\Philipp Koytek <philipp.koytek@me.com>
2016-09-15 18:20:03 -0600

    stretch views to take whole screen

--- app/main.js
+++ app/main.js
@@ -14,20 +14,24 @@ Data.request('data/fifaplayers-top50.json', 'fifaplayers', function(error, data,
     //add group where all brushes will go
     var allBrushMenus = d3.select('.canvas').append('g').classed('all-brush-menus', true);
 
+    var fullSize = d3.select('body').node().getBoundingClientRect();
+    var margin = 30;
+    var viewWidth = fullSize.width/2 - margin*1.5;
+    var viewHeight = fullSize.height/2 - margin*1.5;
     // create charts
-    var scatterplot = new ScatterPlot('likes', 'dislikes', 700, 400, {x: 20, y: 20});
-    var scatterplot2 = new ScatterPlot('wage', 'value', 700, 400, {x: 740, y: 20});
+    var scatterplot = new ScatterPlot('likes', 'dislikes', viewWidth, viewHeight, {x: margin, y: margin});
+    var scatterplot2 = new ScatterPlot('wage', 'value', viewWidth, viewHeight, {x: viewWidth + 2*margin, y: margin});
     scatterplot2.xValue = function(d){
         return d.wage;
     };
     scatterplot2.yValue = function(d){
         return d.value;
     };
-    var barchart = new BarChart('number of players', 700, 400, {x: 20, y: 440});
+    var barchart = new BarChart('number of players', viewWidth, viewHeight, {x: margin, y: viewHeight + 2*margin});
 
     // var parallelcoords = new ParallelCoords(700, 400, {x: 740, y: 20});
     // var listview = new ListView(700, 400, {x: 740, y: 440});
-    DEBUG = new DebugView(700, 400, {x:740, y:440});
+    DEBUG = new DebugView(viewWidth, viewHeight, {x:viewWidth + 2*margin, y:viewHeight + 2*margin});
 
     // add charts to global views object and fill with data
     VIEWS[scatterplot.viewId] = scatterplot.data(data);
