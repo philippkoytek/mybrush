443d44ac57795769b298e9cd5399c880cadad522
AUTOBVT-UPIDP8V\Philipp Koytek <philipp.koytek@me.com>
2016-09-19 19:46:10 -0600

    more views

--- app/main.js
+++ app/main.js
@@ -3,53 +3,95 @@
  */
 
 var DEBUG;
-var DATA;
+var DATA = [];
 var VIEWS = {};
 
-Data.request('data/fifaplayers_0-999.json', 'fifaplayers', function(error, data, key) {
+Data.request('data/fifaplayers-top50.json', 'fifaplayers', function(error, data, key) {
 
     //TODO: positioning of views, make them draggable?
+    var subset = data.filter(function(d){
+        return ['Arsenal', 'FC Barcelona', 'Real Madrid CF', 'FC Bayern Munich', 'Roma'].indexOf(d.club) >= 0;
+    });
     var groupsOf50 = _.chunk(data, 50);
-    DATA = groupsOf50[0];// = [data[0], data[1]];
+    //DATA = groupsOf50[15];// = [data[0], data[1]];
+    for(var i = 0; i < groupsOf50.length; i++){
+        DATA.push(groupsOf50[i][0]);
+        DATA.push(groupsOf50[i][1]);
+    }
+    DATA = data;
 
     //add group where all brushes will go
     var allBrushMenus = d3.select('.canvas').append('g').classed('all-brush-menus', true);
 
     var fullSize = d3.select('body').node().getBoundingClientRect();
     var margin = 30;
-    var viewWidth = fullSize.width/2 - margin*1.5;
-    var viewHeight = fullSize.height/2 - margin*1.5;
+    var viewWidth = (fullSize.width - margin*(4+1))/4; // one margin more than view columns
+    var viewHeight = (fullSize.height - margin*(2+1))/2; // one margin more than view rows
+
+    function viewPosition (xi, yi) {
+        return {
+            x: xi * viewWidth + (xi+1) * margin,
+            y: yi * viewHeight + (yi + 1) * margin
+        };
+    }
+
+
+
     // create charts
-    var scatterplot = new ScatterPlot('x', 'y', viewWidth, viewHeight, {x: margin, y: margin});
+    var scatterplot = new ScatterPlot('Attacking', 'Defending', viewWidth, viewHeight, viewPosition(0,0));
     scatterplot.xValue = function(d){
-        return d.skillProperties[0].sumValue;
+        return d.skillProperties[0].sumValue; //Attacking
         //return d.skillProperties[2].subProperties[1].value; // sprintspeed
-        //return new Date(Date.now() - d.birthdate.$date).getTime() / (1000*3600*24*365); // age
     };
     scatterplot.yValue = function(d){
-        return d.skillProperties[5].sumValue;
-        //return d.skillProperties[2].subProperties[0].value; // acceleration
+        return d.skillProperties[5].sumValue; //Defending
     };
-    var parallelcoords = new ParallelCoords(viewWidth, viewHeight, {x: viewWidth + 2*margin, y: margin});
+    var parallelcoords = new ParallelCoords(viewWidth, viewHeight, viewPosition(1,0));
 
-    var barchart = new BarChart('number of players', viewWidth, viewHeight, {x: margin, y: viewHeight + 2*margin}, {top:20, right:20, bottom:30, left:40});
+    var barchart = new BarChart('number of players', viewWidth, viewHeight, viewPosition(0,1), {top:20, right:20, bottom:30, left:40});
     barchart.keyValue = function(d){ return d.positions[0]; };
 
-    var listview = new ListView(viewWidth, viewHeight, {x:viewWidth + 2*margin, y:viewHeight + 2*margin});
-    //DEBUG = new DebugView(viewWidth, viewHeight, {x:viewWidth + 2*margin, y:viewHeight + 2*margin});
+    var listview = new ListView(viewWidth, viewHeight, viewPosition(1,1));
+
+    var barchart2 = new BarChart('number of players', viewWidth, viewHeight, viewPosition(2,0));
+
+    var scatterplot2 = new ScatterPlot('age', 'acceleration', viewWidth, viewHeight, viewPosition(3,0));
+    scatterplot2.xValue = function(d){
+        return new Date(Date.now() - new Date(d.birthdate.$date)).getTime() / (1000*3600*24*365); // age
+    };
+    scatterplot2.yValue = function(d){
+        return d.skillProperties[2].subProperties[0].value; // acceleration
+    };
+
+    var parallelcoords2 = new ParallelCoords(viewWidth, viewHeight, viewPosition(2,1));
+    parallelcoords2.calcDimensions = function(d){
+        //return ['dislikes', 'likes', 'height', 'weight', 'overallRating', 'value', 'wage'];
+        return d.skillProperties[0].subProperties.map(s => s.title);
+    };
+    parallelcoords2.yValue = function(d, i, dim){
+        //return d[dim];
+        return d.skillProperties[0].subProperties.find(s => s.title === dim).value;
+    };
+
+    var barchart3 = new BarChart('number of players', viewWidth, viewHeight, viewPosition(3,1));
+    barchart3.keyValue = function(d){ return d.nationality; };
 
     // add charts to global views object and fill with data
     VIEWS[scatterplot.viewId] = scatterplot;
     VIEWS[parallelcoords.viewId] = parallelcoords;
     VIEWS[barchart.viewId] = barchart;
     VIEWS[listview.viewId] = listview;
+    VIEWS[barchart2.viewId] = barchart2;
+    VIEWS[scatterplot2.viewId] = scatterplot2;
+    VIEWS[parallelcoords2.viewId] = parallelcoords2;
+    VIEWS[barchart3.viewId] = barchart3;
 
-    scatterplot.data(DATA);
-    parallelcoords.data(DATA);
-    barchart.data(DATA);
-    listview.data(DATA);
+    _.forEach(VIEWS, function(view){
+        view.data(DATA);
+    });
 
     // links and brush menus are above all other elements
     d3.select('.canvas').append('g').classed('links', true);
     allBrushMenus.moveToFront();
 });
+
